0379ce22d0f5024a781724ebbdb8e17fcd68368e
Patrick Winter <patrick.winter@adfinis-sygroup.ch>
2017-02-15 16:22:08 +0100

    refactored domain views into viewset

--- sydns/api/views.py
+++ sydns/api/views.py
@@ -1,11 +1,10 @@
 from api.models import Domain, Record, Zone, User
 from api.serializers import DomainSerializer, RecordSerializer
-from rest_framework import generics, status
+from rest_framework import viewsets, generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework.permissions import IsAuthenticated
-from django.core.exceptions import ObjectDoesNotExist
 
 
 @api_view(['GET'])
@@ -16,9 +15,13 @@ def api_root(request, format=None):
     })
 
 
-class DomainList(generics.ListCreateAPIView):
+class DomainViewSet(viewsets.ModelViewSet):
+    """
+    This viewset provides actions around `domains`.
+    """
     serializer_class = DomainSerializer
     permission_classes = (IsAuthenticated,)
+    lookup_field = "name"
 
     def get_queryset(self):
         '''Only return domains which the user is allowed to manage.
@@ -26,20 +29,7 @@ class DomainList(generics.ListCreateAPIView):
         '''
         owner = User.objects.get(username=self.request.user.username)
         allowed_zones = [zone.id for zone in Zone.objects.filter(owner=owner.id)]
-        return Domain.objects.filter(pk__in=allowed_zones)
 
-
-class DomainDetail(generics.RetrieveUpdateAPIView):
-    serializer_class = DomainSerializer
-    permission_classes = (IsAuthenticated,)
-    lookup_field = 'name'
-
-    def get_queryset(self):
-        '''Only return domains which the user is allowed to manage.
-
-        '''
-        owner = User.objects.get(username=self.request.user.username)
-        allowed_zones = [zone.id for zone in Zone.objects.filter(owner=owner.id)]
         return Domain.objects.filter(pk__in=allowed_zones)
 
 
