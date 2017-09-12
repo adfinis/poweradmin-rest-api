256be258f23e54868663e23bd0ed92a84f706626
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2017-09-12 10:47:12 +0200

    Allow root access to mysql
    Test needs to setup test database hence root is needed.

--- powerdns/settings_ci.py
+++ powerdns/settings_ci.py
@@ -4,7 +4,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'powerdns',
-        'USER': 'travis',
+        'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
